f06db4917dfd0bbba44237f5b41925ac7b5b942f
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-05-24 08:04:57 +1200

    fixed the onChange error
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import './index.scss'
-import inputs from '../../db/data'
 import FormInputs from '../FormInputs/FormInputs'
 
 const Form = () => {
@@ -11,12 +10,63 @@ const Form = () => {
 		password: '',
 		confirmPassword: '',
 	})
-	const handleChange = (e) => {
-		setValues({ ...values, [e.target.name]: e.target.value })
-	}
 
+	const inputs = [
+		{
+			id: 1,
+			name: 'username',
+			type: 'text',
+			placeholder: 'Username',
+			errorMessage:
+				'Username should be 3-16 characters and should not include any special characters!',
+			label: 'Username',
+			pattern: '^[A-Za-z0-9]{3,16}$',
+			required: true,
+		},
+		{
+			id: 2,
+			name: 'email',
+			type: 'email',
+			placeholder: 'Email',
+			errorMessage: 'It should be a valid email address!',
+			label: 'Email',
+			required: true,
+		},
+		{
+			id: 3,
+			name: 'birthday',
+			type: 'date',
+			placeholder: 'Birthday',
+			label: 'Birthday',
+		},
+		{
+			id: 4,
+			name: 'password',
+			type: 'password',
+			placeholder: 'Password',
+			errorMessage:
+				'Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!',
+			label: 'Password',
+			pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+			required: true,
+		},
+		{
+			id: 5,
+			name: 'confirmPassword',
+			type: 'password',
+			placeholder: 'Confirm Password',
+			errorMessage: 'Password do not match!',
+			label: 'Confirm Password',
+			pattern: values.password,
+			required: true,
+		},
+	]
 	const handleSubmit = (e) => {
 		e.preventDefault()
+
+	}
+	const onChange = (e) => {
+		setValues({ ...values, [e.target.name]: e.target.value })
 	}
 	return (
 		<>
@@ -27,7 +77,7 @@ const Form = () => {
 						key={input.id}
 						{...input}
 						value={values[input.name]}
-						onChange={handleChange}
+						onChange={onChange}
 					/>
 				))}
 				<button className='btn'>Submit</button>
--- src/Components/FormInputs/FormInputs.js
+++ src/Components/FormInputs/FormInputs.js
@@ -2,23 +2,23 @@ import React, { useState } from 'react'
 import './index.scss'
 
 const FormInputs = (props) => {
-	const [isFocused, setIsFocused] = useState(false)
-	const { label, errorMessage, handleChange, id, ...inputProps } = props
+	const [focused, setFocused] = useState(false)
+	const { label, errorMessage, onChange, id, ...inputProps } = props
 
 	const handleFocus = (e) => {
-		setIsFocused(true)
+		setFocused(true)
 	}
 	return (
 		<div className='form-group'>
 			<label>{label}</label>
 			<input
 				{...inputProps}
-				onChange={handleChange}
+				onChange={onChange}
 				onBlur={handleFocus}
 				onFocus={() =>
-					inputProps.name === 'confirmPassword' && setIsFocused(true)
+					inputProps.name === 'confirmPassword' && setFocused(true)
 				}
-				isFocused={isFocused.toString()}
+				focused={focused.toString()}
 			/>
 			<span>{errorMessage}</span>
 		</div>
